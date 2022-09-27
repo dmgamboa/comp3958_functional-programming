8ed04b67c01fa353eb82f440f9376f4a5fe6d53d
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2022-09-26 20:17:53 -0700

    LAB2 Prettier code

--- lab2.ml
+++ lab2.ml
@@ -13,17 +13,15 @@ let min_elt l = List.fold_left
     Returns a list l with the first occurrence of x removed *)
 let rec remove x = function
     | [] -> []
-    | y::ys ->
-        if x = y then ys
-        else y::(remove x ys)
+    | y::ys when x <> y -> y::(remove x ys)
+    | _::ys -> ys
 
 (* c. val selection_sort : 'a list -> 'a list
     Returns sorted list by selecting the smallest element of the list
     and moving it to the front *)
 let rec selection_sort = function
     | [] -> []
-    | x::xs as l ->
-        let m = min_elt(l) in
+    | x::xs as l -> let m = min_elt(l) in
         m::(selection_sort @@ remove m l)
 
 
@@ -39,7 +37,7 @@ let group l =
         | (x::xs) ->
             match x with
             | y::ys as l when y = curr -> (curr::l)::xs
-            | [] | _::_ -> [curr]::acc
+            | _ -> [curr]::acc
     ) l []
 
 (* b. val frequencies : 'a list -> ('a * int) list 
@@ -47,12 +45,12 @@ let group l =
     and the second element is the number of times it appears in the list *)
 let frequencies l =
     let sorted_list = List.sort (fun x y -> 
-        if x < y then - 1 else 1
+        if x < y then 1 else (-1)
     ) l
-    in List.fold_right (fun curr acc ->
+    in List.fold_left (fun acc curr ->
         match acc with
         | [] -> [(curr, 1)]
         | (value, freq)::xs as l ->
             if curr = value then (value, (freq + 1))::xs
             else (curr, 1)::l
-    ) sorted_list []
+    ) [] sorted_list
