cf1e999579a0b381ab410251bab49f85123fc2c2
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2022-10-30 18:42:01 -0700

    Add comments

--- lab4/primes.ml
+++ lab4/primes.ml
@@ -3,12 +3,17 @@
 module PermutationMap = struct
     include Map.Make(String)
 
+    (* val to_sorted_string : key -> key
+    Returns a string with each character sorted based on its ASCII value *)
     let to_sorted_string s =
         List.init (String.length s) (String. get s)
         |> List.sort Char.compare
         |> List.to_seq
         |> String.of_seq
     
+    (* val map_by_permutation :  int t -> key list -> int t
+    Given a list of strings, returns a map with key-value pairs corresponding 
+    to the sets of permutations within that list *)
     let rec map_by_permutation m l =
         match l with
         | [] -> m
@@ -20,6 +25,8 @@ module PermutationMap = struct
             with
             | Not_found -> map_by_permutation (m |> add key 1) xs
 
+    (* val get_max_perms : int t -> int 
+    Returns the key with the highest stored int value *)
     let get_max_perms m = fold (fun k v acc -> if v > acc then v else acc) m 0
 end
 
@@ -36,4 +43,4 @@ let () =
     get_input ()
     |> PermutationMap.map_by_permutation PermutationMap.empty
     |> PermutationMap.get_max_perms
-    |> Printf.printf "The size of the largest set of permutations is %d\n"
+    |> Printf.printf "The largest set of permutations has size %d\n"
